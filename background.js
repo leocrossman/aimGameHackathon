47885eb8bd5f5d624acf80f7541d09526bf4f957
leocrossman <lkcrossman@gmail.com>
2021-12-02 14:25:56 -0500

    added accuracy

--- background.js
+++ background.js
@@ -1,11 +1,7 @@
-function reddenPage() {
-  document.body.style.backgroundColor = 'red';
-}
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: main,
-    // function: reddenPage,
   });
 });
 function main() {
@@ -13,10 +9,18 @@ function main() {
   const originalHtml = document.documentElement.innerHTML;
   const game = {};
   game.score = 0;
-  game.accuracy = 0;
+  game.hitCount = 0;
+  game.totalClicks = 0;
+  game._accuracy = 0;
+  game.accuracyPercent = `${100}%`; // init to 100%
   const constrainToWindow = 9;
   // transformations
   const rotate360 = [{ transform: 'rotate(360deg)' }];
+  // add sound url
+  let url = chrome.runtime.getURL('hitmarker.mp3');
+  let hitMarker = new Audio(url);
+  hitMarker.volume = 1;
+  console.log(hitMarker);
 
   game.transformations = [rotate360];
 
@@ -38,18 +42,16 @@ function main() {
     return [...elements].flat(Infinity);
   };
   const allElements = game.getAllElements();
-  console.log(allElements);
-  //document.body.innerHTML = '';
   document.body.innerHTML = '';
   document.body.style.cursor = 'crosshair';
   allElements.forEach((element) => {
     element.style.cursor = 'crosshair';
     document.body.appendChild(element);
   });
-  console.log(allElements);
   //console.log('still hasnt started')
-  //document.body.appendChild()
+
   const maxSize = 200;
+
   // clean all elements (delete: <style>, etc)
 
   game.filterElements = function (elements) {
@@ -84,15 +86,29 @@ function main() {
   // score stuff
   const scoreText = document.createElement('h1');
   scoreText.innerText = game.score;
-  scoreText.style.margin = 'auto';
-  scoreText.style.width = '100%';
+  //scoreText.style.margin = 'auto';
+  //scoreText.style.width = '100%';
   scoreText.style.position = 'fixed';
   scoreText.style.top = '50%';
+  scoreText.style.left = '80%';
   scoreText.style.fontSize = '100px';
   scoreText.style.textAlign = 'center';
   scoreText.style.zIndex = '999999';
+  const origColor = scoreText.style.color;
 
   document.body.appendChild(scoreText);
+
+  // accuracy stuff
+  const accuracyText = document.createElement('h1');
+  accuracyText.innerText = game.accuracyPercent;
+  accuracyText.style.position = 'fixed';
+  accuracyText.style.top = '80%';
+  accuracyText.style.left = '80%';
+  accuracyText.style.fontSize = '100px';
+  accuracyText.style.textAlign = 'center';
+  accuracyText.style.zIndex = '999998';
+
+  document.body.appendChild(accuracyText);
 
   // for-each element...
   allElements.forEach((element, idx) => {
@@ -103,6 +119,17 @@ function main() {
     element.style.zIndex = `${idx}`;
 
     element.removeAttribute('href');
+
+    // constrains elements to respective width and height
+    element.style.width = 'auto';
+    element.style.height = 'auto';
+
+    element.style.margin = '0';
+
+    // removes videos?
+    if (element.tagName === 'IFRAME') {
+      element.setAttribute('src', '');
+    }
 
     // downup animation
     const tenPercentOfElements = Math.random() < 0.1;
@@ -145,28 +172,76 @@ function main() {
     const anim = getRandomAnim();
     // const anim = getAnim(element, idx);
     element.animate(anim[0], anim[1]);
+
+    //ON CLICK OF EACH ELEMENT
     // add a click eventlistener to each element
     element.addEventListener('click', function (event) {
+      // if (element.tagName === 'BODY') {
+      //   console.log('body clicked');
+      // }
       element.remove();
+      game.totalClicks++;
       game.score++;
+      game.hitCount++;
+      game._accuracy = game.hitCount / game.totalClicks;
+      game.accuracyPercent = `${Math.round(game._accuracy * 100)}%`;
+
       scoreText.innerText = game.score;
+      accuracyText.innerText = game.accuracyPercent;
+
+      //ADDED SOUND!
+      hitMarker.play();
+
+      // flash green text or body
+      //document.body.style.backgroundColor = 'green';
+      scoreText.style.color = 'green';
+      setTimeout(function () {
+        scoreText.style.color = origColor;
+        document.body.style.backgroundColor = 'white';
+      }, 200);
+      //hitMarker.play();
+      event.stopPropagation();
     });
   });
 
+  //if click body, decrease point
+  // flash red text or body
+  document.body.addEventListener('click', function (event) {
+    game.totalClicks++;
+    game.score--;
+    game._accuracy = game.hitCount / game.totalClicks;
+    game.accuracyPercent = `${game._accuracy}%`;
+
+    scoreText.innerText = game.score;
+    accuracyPercent.innerText = game.accuracyPercent;
+
+    // flash red text or body
+    //document.body.style.backgroundColor = 'red';
+    scoreText.style.color = 'red';
+    setTimeout(function () {
+      document.body.style.backgroundColor = 'white';
+      scoreText.style.color = origColor;
+    }, 100);
+  });
+
+  document.body.overflowX = 'hidden';
+
+  // ! TIMER ELEMENT WAS BLOCKING ELEMENTS UNDER/NEXT TO IT FROM BEING CLICKED BC IT IS 100% WIDTH
   // create timer element
   const timer = document.createElement('h1');
-  timer.style.margin = 'auto';
-  timer.style.width = '100%';
+  //timer.style.margin = 'auto';
+  //timer.style.width = '100%';
   timer.style.position = 'fixed';
   timer.style.top = '10%';
-  timer.style.left = '5%';
+  timer.style.left = '80%';
   timer.style.fontSize = '100px';
-  timer.style.textAlign = 'center';
+  timer.style.textAlign = 'right';
   timer.style.zIndex = '1000000';
+  timer.innerHTML = '60s';
   document.body.appendChild(timer);
 
   // timer stuff
-  var countDownDate = Number(Date.now()) + 10000;
+  var countDownDate = Number(Date.now()) + 61000;
   const x = setInterval(function () {
     var now = Number(Date.now());
 
@@ -180,6 +255,10 @@ function main() {
     } else {
       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+      //change color to red at 10s
+      if (seconds < 11) {
+        timer.style.color = 'red';
+      }
       // Display the result in the element with id="demo"
       timer.innerHTML = seconds + 's ';
     }
