7ab47b4b22d607c1eacbd26ed68b8f5199eda5b0
leocrossman <lkcrossman@gmail.com>
2021-12-01 19:42:47 -0500

    dinner XD

--- background.js
+++ background.js
@@ -5,6 +5,34 @@ function reddenPage() {
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: reddenPage,
+    function: main,
+    // function: reddenPage,
   });
 });
+
+function main() {
+  const game = {};
+  game.score = 0;
+  game.accuracy = 0;
+  game.transformations = [];
+
+  // get all elements from current tab
+  game.getAllElements = function () {
+    const elements = document.body.getElementsByTagName('*');
+    return elements;
+  };
+  const allElements = game.getAllElements();
+  // clean all elements (delete: <style>, etc)
+  game.filterElements = function (elements) {
+    // filter if width and length != 0
+    // filter if style element
+    return elements.filter((el, idx) => {
+      if (el.offsetWidth > someNumber && el.off) {
+        return el;
+      }
+    });
+  };
+  const filteredElements = game.filterElements(allElements);
+
+  // assign unique z-index to each element on the page
+}
